9b0261093aa56ae3bcbe6938ea6f0cf6c321cae8
AmrFiqi <amr.h.elfiqi@gmail.com>
2023-07-16 00:14:39 +0300

    Added folders designed to the mvvm pattern

--- Movie Trending/AppDelegate.swift
+++ Movie Trending/AppDelegate.swift
@@ -9,20 +9,25 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        showMainView()
+        return true
+    }
+    
+    func showMainView() {
+        // Set main view controller
         let MainVC = MainViewController()
         let navigationController = UINavigationController(rootViewController: MainVC)
         
         self.window = UIWindow(frame: UIScreen.main.bounds)
         self.window?.rootViewController = navigationController
         self.window?.makeKeyAndVisible()
-        return true
     }
-
+    
 }
 
